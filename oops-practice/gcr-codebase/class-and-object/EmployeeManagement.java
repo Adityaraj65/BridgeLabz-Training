b5ae5a2758608de5e173b83fbea17be60f89ceb0
Aditya <ar850732@gmail.com>
2025-12-30 17:36:32 +0530

    [Aditya Raj] Add .Added class and object programs

---
+++ oops-practice/gcr-codebase/class-and-object/EmployeeManagement.java
@@ -0,0 +1,22 @@
+public class EmployeeManagement{
+    public static void main(String args[]){
+        Employee employee = new Employee("Rohan","1",50000);
+        employee.display();
+    }
+}
+class Employee{
+    String name;
+    String id;
+    int salary;
+    public Employee(String name, String id ,int salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void display(){
+        System.out.println("Employee Name "+this.name);
+        System.out.println("Employee id "+this.id);
+        System.out.println("Employee Salary "+this.salary); 
+    }
+}

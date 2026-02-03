cd64c3f0d27698ab2385bf584bab304c99ff3661
Aditya <ar850732@gmail.com>
2026-02-03 16:31:47 +0530

    [Aditya raj] Add. Added annotation problem

---
+++ collections-practice/gcr-codebase/annotation/override/OverrideAnnotation.java
@@ -0,0 +1,13 @@
+package annotation.override;
+
+public class OverrideAnnotation {
+    public static void main(String[] args) {
+
+        // Creating object of Dog class
+        Dog dog = new Dog();
+
+        // Calling overridden method
+        dog.makeSound();
+    }
+}
+

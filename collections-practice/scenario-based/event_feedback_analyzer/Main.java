edbf0bdde37be66358a06111dbe5e053b9037aec
Aditya <ar850732@gmail.com>
2026-02-13 21:43:04 +0530

    [Aditya raj] refactor remove .unused import in  event feedback problem

--- collections-practice/scenario-based/event_feedback_analyzer/Main.java
+++ collections-practice/scenario-based/event_feedback_analyzer/Main.java
@@ -1,7 +1,4 @@
 package event_feedback_analyzer;
-
-import java.util.Arrays;
-import java.util.List;
 public class Main {
     public static void main(String[] args) {
         EventUtil analyzer = new EventUtil();

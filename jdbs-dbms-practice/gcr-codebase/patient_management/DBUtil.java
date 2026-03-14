9607499df08e9631a06bf2a17ceb045322e65854
Aditya <ar850732@gmail.com>
2026-03-14 11:33:21 +0530

    [Aditya raj] refactor .

--- jdbs-dbms-practice/gcr-codebase/patient_management/DBUtil.java
+++ jdbs-dbms-practice/gcr-codebase/patient_management/DBUtil.java
@@ -5,7 +5,8 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/health_clinic_app";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "" +
+            "";
     // This method gives a fresh DB connection whenever needed
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

05b04baeeeece037b53bf31a2ff4d727c113b159
Aditya <ar850732@gmail.com>
2026-07-31 16:55:35 +0530

    [Aditya raj]Add .added hackerrank problem in refresher training branch

---
+++ Refresher-Training/day1/ArrayManipulation.java
@@ -0,0 +1,83 @@
+package day1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+    // Write your code here
+    long[] arr = new long[n + 2];
+   
+    for (List<Integer> query : queries) {
+        int a = query.get(0);
+        int b = query.get(1);
+        int k = query.get(2);
+        arr[a] += k;
+        if (b + 1 <= n) {
+            arr[b + 1] -= k;
+        }
+    }
+
+    long max = 0;
+    long sum = 0;
+   
+    for (int i = 1; i <= n; i++) {
+        sum += arr[i];
+        if (sum > max) {
+            max = sum;
+        }
+    }
+    return max;
+    }
+}
+
+public class ArrayManipulation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> queriesRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowTempItems[j]);
+                queriesRowItems.add(queriesItem);
+            }
+
+            queries.add(queriesRowItems);
+        }
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Refresher-Training/day1/SherlocksAndAnagram.java
@@ -0,0 +1,56 @@
+package day1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
+
+public class SherlocksAndAnagram {
+	 public static int sherlockAndAnagrams(String s) {
+		    // Write your code here
+		    HashMap<String, Integer> map = new HashMap<>();
+		    int totalPairs = 0;
+		    for (int i = 0; i < s.length(); i++) {
+		        for (int j = i + 1; j <= s.length(); j++) {
+		            String sub = s.substring(i, j);
+		            char[] charArray = sub.toCharArray();
+		            Arrays.sort(charArray);
+		            String sortedSub = new String(charArray);
+		            int occurrence = map.getOrDefault(sortedSub, 0);
+		            totalPairs += occurrence;
+		            map.put(sortedSub, occurrence + 1);
+		        }
+		    }
+		    return totalPairs;
+
+		    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = sherlockAndAnagrams(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+

7633a0e31f3200fc583ac6f0c65e9cb3f001a954
Aditya <ar850732@gmail.com>
2026-08-10 12:06:00 +0530

    [Aditya raj] refactor. reverseString hackerrank problem using recursion in refresher-training branch

--- Refresher-Training/day7/JavaStringReverse.java
+++ Refresher-Training/day7/JavaStringReverse.java
@@ -3,21 +3,35 @@ package day7;
 import java.util.Scanner;
 
 public class JavaStringReverse {
-
+    public static boolean isPlaindrome(String a,int i,int j){
+        if(i>=j){
+            return true;
+        }
+        if(a.charAt(i)!=a.charAt(j)){
+            return false;
+        }
+        return isPlaindrome(a,i+1,j-1);
+    }
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        String reverse=new StringBuilder(A).reverse().toString();
-        if(A.equals(reverse)){
+        int i=0;
+        int j=A.length()-1;
+        boolean result=isPlaindrome(A, i, j);
+        if(result){
             System.out.println("Yes");
         }else{
             System.out.println("No");
         }
+       
+       
         
     }
 }
+
+
+
 
 
 
